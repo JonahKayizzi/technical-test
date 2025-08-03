692662fda347a0c21eda6050e0d4f15659ba0691
jonahkayizzi <jonahkayizzi@gmail.com>
2025-08-03 12:25:48 +0300

    Fix styling isses

--- src/types/jest-dom.d.ts
+++ src/types/jest-dom.d.ts
@@ -1,13 +1,13 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 declare global {
-    namespace jest {
-        interface Matchers<R> {
-            toBeInTheDocument(): R
-            toHaveTextContent(text: string): R
-            toBeDisabled(): R
-            toHaveClass(className: string): R
-            toHaveAttribute(attr: string, value?: string): R
-        }
+  namespace jest {
+    interface Matchers<R> {
+      toBeInTheDocument(): R;
+      toHaveTextContent(text: string): R;
+      toBeDisabled(): R;
+      toHaveClass(className: string): R;
+      toHaveAttribute(attr: string, value?: string): R;
     }
-} 
+  }
+}
